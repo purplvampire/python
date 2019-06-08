20e7806fbc2e34eed45706baa1281f5a1179f97c
Hsin Chang Chen <purplvampire@gmail.com>
2019-06-08 16:45:48 +0800

    Write CSV

---
+++ writeCsv.py
@@ -0,0 +1,15 @@
+import csv
+outputFile = open('output.csv', 'w', newline='')
+outputWriter = csv.writer(outputFile)
+outputWriter.writerow(['spam', 'eggs', 'bacon', 'bam'])
+outputWriter.writerow(['Hello, world!', 'eggs', 'bacon', 'ham'])
+outputWriter.writerow([1, 2, 3.141592, 4])
+outputFile.close()
+
+csvFile = open('example.tsv', 'w', newline='')
+csvWriter = csv.writer(csvFile, delimiter='\t', lineterminator='\n\n')
+csvWriter.writerow(['apples', 'oranges', 'grapes'])
+csvWriter.writerow(['eggs', 'bacon', 'ham'])
+csvWriter.writerow(['spam', 'spam', 'spam', 'spam', 'spam', 'spam'])
+csvFile.close()
+
