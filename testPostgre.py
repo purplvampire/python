c25d9cdf6d1cbc670dde95b6d0528f676dfccb49
Hsin Chang Chen <purplvampire@gmail.com>
2020-08-30 21:22:38 +0800

    Connect to PostgreSQL

---
+++ testPostgre.py
@@ -0,0 +1,26 @@
+#! python3
+import psycopg2 as pg2 
+
+# Connect to an existing database
+conn = pg2.connect(database='dvdrental', user='postgres',password='Jason1981')
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a command: this creates a new table
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+
+# Pass data to fill a query placeholders and let Psycopg perform
+# the correct conversion (no more SQL injections!)
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+
+# Query the database and obtain data as Python objects
+cur.execute("SELECT * FROM test;")
+cur.fetchone()
+
+# Make the changes to the database persistent
+conn.commit()
+
+# Close communication with the database
+cur.close()
+conn.close()
