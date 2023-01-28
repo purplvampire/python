8c2cbe2058613ff61ff657e846ca273ce0d86f6a
Hsin Chang Chen <purplvampire@gmail.com>
2023-01-28 14:17:54 +0800

    Learning class inheritance

--- OOB.py
+++ OOB.py
@@ -65,3 +65,50 @@ print(light)
 
 # 呼叫staticmethod
 book.static_method()
+
+# 物件繼承
+print("-----Object inheritance-----")
+
+# 建立裝置物件
+class Device:
+    def __init__(self, name, connected_by):
+        self.name = name
+        self.connected_by = connected_by
+        self.connected = True
+
+    def __str__(self):
+        return f"Device {self.name!r} ({self.connected_by})"
+
+    def disconnected(self):
+        self.connected = False
+        print("Disconnected.")
+
+printer = Device("Printer", "USB")
+print(printer)
+printer.disconnected()
+
+print("-----Printer Object-----")
+# 建立印表機物件
+class Printer(Device):  # 套用父系物件
+    def __init__(self, name, connected_by, capacity):
+        super().__init__(name, connected_by)    # 用super()繼承父系物件
+        self.capacity = capacity
+        self.remaining_pages = capacity
+
+    def __str__(self):
+        return f"{super().__str__()} ({self.remaining_pages} pages remaining.)"
+    
+    def print(self, pages):
+        if not self.connected:  # 已繼承可直接使用父系物件的method
+            print("Your printer is not connected.")
+            return
+        print(f"Printing {pages} pages.")
+        self.remaining_pages -= pages   # 減去已使用的紙張數
+
+printer = Printer("Printer", "USB", 500)
+printer.print(20)
+print(printer)
+printer.disconnected()
+printer.print(10)
+
+
